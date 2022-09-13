6cbede0921bae91bee92b2574e8495b983832afb
Akshad-Agarwal <106867353+Akshad-Agarwal@users.noreply.github.com>
2022-09-13 10:55:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,66 @@
+// https://teachablemachine.withgoogle.com/models/_CZegzj-r/
+
+function start_classification() {
+    navigator.mediaDevices.getUserMedia({ audio: true });
+    classifier = ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/_CZegzj-r/model.json", model_loaded_boss);
+}
+
+function model_loaded_boss() {
+    console.log("MR");
+    classifier.classify(gotresult);
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+
+    else {
+        console.log(results);
+        random_number_r = Math.floor(Math.random() * 255) + 1;
+        random_number_g = Math.floor(Math.random() * 255) + 1;
+        random_number_b = Math.floor(Math.random() * 255) + 1;
+
+        document.getElementById("result_label").innerHTML = 'I can hear -' + results[0].label;
+
+        document.getElementById("Accuracy_label").innerHTML = 'Accuracy -' + (results[0].confidence * 100).toFixed(2) + "%";
+
+        document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_r + ")";
+
+        document.getElementById("Accuracy_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_r + ")";
+
+        img = document.getElementById("a1");
+        img1 = document.getElementById("a2");
+        img2 = document.getElementById("a3");
+        img3 = document.getElementById("a4");
+
+        if (results[0].label == "Clap") {
+            img.src = "aliens-01.gif";
+            img1.src = "aliens-02.png";
+            img2.src = "aliens-03.png";
+            img3.src = "aliens-04.png";
+        }
+
+        else if (results[0].label == "Bell") {
+            img.src = "aliens-01.png";
+            img1.src = "aliens-02.gif";
+            img2.src = "aliens-03.png";
+            img3.src = "aliens-04.png";
+        }
+
+        else if (results[0].label == "Snapping") {
+            img.src = "aliens-01.png";
+            img1.src = "aliens-02.png";
+            img2.src = "aliens-03.gif";
+            img3.src = "aliens-04.png";
+        }
+
+        else{
+            img.src = "aliens-01.png";
+            img1.src = "aliens-02.png";
+            img2.src = "aliens-03.png";
+            img3.src = "aliens-04.gif";
+        }
+
+    }
+}
